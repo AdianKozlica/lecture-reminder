975a0de83ba8ef32e239116ffb6e1bedeee4925c
AdianKozlica <kozlicaadian@gmail.com>
2023-10-11 14:58:56 +0200

    Update: update imports, and implement function for getting day and time

--- lecture-reminder.py
+++ lecture-reminder.py
@@ -1,6 +1,18 @@
+from datetime import datetime
+
 import os
 import sys
+import json
 import notify2
+
+def get_current_day_and_time():
+    date = datetime.now()
+
+    day = date.strftime("%a")
+    hours_minutes = date.strftime("%H:%M")
+
+    return day, hours_minutes
+
 
 def main():
     if len(sys.argv) < 2:
@@ -12,6 +24,9 @@ def main():
     if not os.path.isfile(json_file):
         sys.stderr.write("File doesn't exist!\n")
         exit(1)
+
+    with open(json_file) as file:
+        json_dict = json.load(file)
 
 if __name__ == "__main__":
     main()
