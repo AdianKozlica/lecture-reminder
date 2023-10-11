cf432a3d49923e776098d46c60d7d7d671dbfb82
AdianKozlica <kozlicaadian@gmail.com>
2023-10-11 12:31:03 +0200

    Update: Added checking whether the file exists and added exit codes

--- lecture-reminder.py
+++ lecture-reminder.py
@@ -5,7 +5,13 @@ import notify2
 def main():
     if len(sys.argv) < 2:
         sys.stderr.write("Missing json file path!\n")
-        exit()
+        exit(1)
+
+    json_file = sys.argv[1]
+
+    if not os.path.isfile(json_file):
+        sys.stderr.write("File doesn't exist!\n")
+        exit(1)
 
 if __name__ == "__main__":
     main()
