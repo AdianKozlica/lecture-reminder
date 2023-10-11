b6bb3dda40a1d4da08f5df1ef2dab3ac1f6ab984
AdianKozlica <kozlicaadian@gmail.com>
2023-10-11 15:52:38 +0200

    Update: update imports, set icon path and implement rest of functionality

--- lecture-reminder.py
+++ lecture-reminder.py
@@ -1,9 +1,13 @@
 from datetime import datetime
+from pathlib import Path
 
 import os
 import sys
 import json
+import time
 import notify2
+
+ICON_PATH = str(Path(__file__).parent / "img/book.png")
 
 def get_current_day_and_time():
     date = datetime.now()
@@ -27,6 +31,22 @@ def main():
 
     with open(json_file) as file:
         json_dict = json.load(file)
+
+    notify2.init("lecture-reminder")
+
+    while True:
+        time.sleep(1)
+
+        day, hours_minutes = get_current_day_and_time()
+        lecture = json_dict.get(day).get(hours_minutes)
+
+        if not lecture:
+            continue
+        
+        n = notify2.Notification("Lecture", f"Lecture {lecture} is beginning", ICON_PATH)
+        n.show()
+
+        time.sleep(60)
 
 if __name__ == "__main__":
     main()
